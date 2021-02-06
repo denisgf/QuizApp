dc7b40824b96e5af9403159edbbd1920e2f17bb3
Denis <petitdenis@gmail.com>
2021-02-05 22:31:53 -0500

    Adding some style - responsive design - stable version v1.0

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using QuizApp.Models;
 using QuizApp.Utils;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace QuizApp.Controllers
 {
+    [Authorize]
     public class GameController : Controller
     {
         private const int TOTAL_PERCENT = 100;
